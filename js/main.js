5029f427e50086ed23cd152a0e932097efe21824
douglas-g-o <doug_g.o@hotmail.com>
2023-02-05 22:51:06 -0300

    implementacao calculo IMC

--- js/main.js
+++ js/main.js
@@ -8,8 +8,40 @@ form.addEventListener('submit', function (e) {
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
-    if (!peso)
+    if (!peso) {
+        setResultado ('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado ('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
 });
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1',
+    'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.39) return nivel [5]; 
+    if (imc >= 34.9) return nivel [4]; 
+    if (imc >= 29.9) return nivel [3];
+    if (imc >= 24.9) return nivel [2];
+    if (imc >= 18.5) return nivel [1];
+    if (imc < 18.5)  return nivel [0];
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
 
 function criaP () {
     const p = document.createElement('p');
@@ -17,9 +49,19 @@ function criaP () {
     
 }
 
-function setResultado (msg) {
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
+
     const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
     
 }
